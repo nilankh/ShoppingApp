b240c2b95d5c77283d2377df90449cf3223c7d26
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-23 00:51:03 +0530

    write routed for 404 page

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not FOund</h1>');
+});
+
+
 // Listen for requests
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server, ${err}`)};
