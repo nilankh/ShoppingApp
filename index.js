510c617ab2759c33ceb7dc80bf3eb3804111e35b
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-23 23:43:13 +0530

    completed all code to ejs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const path = require('path');
-const express = require('express');
 
+const express = require('express');
 const bodyParser = require('body-parser');
 const port = 8000;
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
