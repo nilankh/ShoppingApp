b1c535156a1d4da45436e1808d621acca27d1449
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-24 00:15:58 +0530

    completed controllers

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const port = 8000;
 const app = express();
 
@@ -17,9 +20,7 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page not Found'});
-});
+app.use(errorController.get404);
 
 
 // Listen for requests
