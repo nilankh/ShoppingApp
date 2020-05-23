8d8ca974ea57601ef46d5a3b117f3669df5c7f93
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-05-24 00:01:06 +0530

    created our controller folder and created first controller

--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,11 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const products = [];
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
